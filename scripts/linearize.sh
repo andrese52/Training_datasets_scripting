3479a976400d8c2c4e5bceaa27df2d1787c6a278
andrese52 <andres.espindolac@gmail.com>
2018-03-15 22:18:27 -0500

    add linearize script

--- scripts/linearize.sh
+++ scripts/linearize.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 input=$1
-output=linearized_$1.fasta
+output=linearized_$1
 awk '/^>/ {printf("%s%s\t",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' < $input > $output
